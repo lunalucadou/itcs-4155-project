c92ab4a144832fd97f6a5c997b50e4f69fcb8e88
blackBoxMod <davis.gamer07@gmail.com>
2020-04-02 22:08:25 -0400

    Corrected components 4

--- src/components/accesspoints.component.js
+++ src/components/accesspoints.component.js
@@ -11,6 +11,31 @@ export default class AccessPoints extends Component {
     // You will need to see how and what to call in order to get the information from the /get route, or else ...
     // ... what was the point of making the route? Surely, it must be used here or else where, though if else where ...
     // ... I don't know where that would be
+    /*
+    constructor(props){
+        // Must call the super when using a sub-class
+        super(props)
+
+        // Represents the fields in the JSON model
+        // Using state is how you will make variables in React instead of saying let point = "xyx"
+        this.state = {
+            id: '';
+            latitude: '',
+            logitude: '',
+            time: = new Date()
+        }
+
+        // This is where you will add methods to update properties of the state
+        // Irrelavent example
+        onGetPointatDate(e){
+            this.setState({
+                if(time > x){
+                    render...
+                }
+            })
+        }
+    }
+    */
 
 
     render() {
