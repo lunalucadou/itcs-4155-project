72605523fc630c38e9d918b359f531b749e5938e
blackBoxMod <davis.gamer07@gmail.com>
2020-04-02 21:18:13 -0400

    Corrected components 2

--- src/components/accesspoints.component.js
+++ src/components/accesspoints.component.js
@@ -8,6 +8,9 @@ export default class AccessPoints extends Component {
 
 
     // Up here is where you will get the information from mongoose to use in the component
+    // You will need to see how and what to call in order to get the information from the /get route, or else ...
+    // ... what was the point of making the route? Surely, it must be used here or else where, though if else where ...
+    // ... I don't know where that would be
 
 
     render() {
