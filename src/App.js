7556918e741a934026314f5b16a619f354f1f21c
bpickard <bpickard@uncc.edu>
2020-04-06 17:03:25 -0400

    built googles maps component into react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
-
 import Map from "./components/map.component.js"
+import { GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps"
+
 
 function App() {
   return (
     <Router>
-    <Route path="/" exact component={Map} />
-    <Route path="/accesspoint" component={Map} />
-    <Route path="/accesspoint/get" component={Map} />
-    <Route path="/accesspoint/add" component={Map} />
+      <div style = {{width: '100vw', height: '100vh'}}>
+        <Map
+            true
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
     </Router>
   );
 }
--- src/components/map.component.js
+++ src/components/map.component.js
@@ -1,99 +1,16 @@
-// Same across all components
-import React, { Component } from 'react';
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-
-// Same with different names across all components
-export default class Map extends Component {
-    /*
+import React from 'react';
+import { GoogleMap, Marker, withGoogleMap, withScriptjs} from 'react-google-maps';
 
 
+const Map = withScriptjs(withGoogleMap((props) => 
+    
+    <GoogleMap
+        defaultZoom={10}
+        defaultCenter={{lat:35.305704, lng:-80.732347}}
+        >
+    {props.isMarkerShown && <Marker position={{lat:35.305704, lng:-80.732347}} />}
+    </GoogleMap>
+    ));     
 
 
-
-    // Seen from below, you have to bind the 'this' keywords to all of the methods that use it
-    this.GetAllPoints = this.GetAllPoints.bind(this)
-    this.ShowPointByDate = this.ShowPointByDate.bind(this)
-    this.HidePointByDate = this.HidePointByDate.bind(this)
-    this.PostPoint = this.PostPoint.bind(this)
-
-
-
-
-    constructor(props){
-        // Must call the SUPER when using a sub-class
-        super(props)
-
-        // This represents class properties, which will be used/rendered in the below HTML/JSX
-        // Using state is how you will make variables in React instead of saying: var point = "xyx"
-        this.state = {
-            accesspoint: []
-        }
-
-
-
-
-
-        // This is where you will add METHODS to update properties of the state
-        // Examples
-
-        // This is a "life cycle" method, this will automatically be called before the page renders the below HTML
-        componentDidMount(){
-            // If we want to test before loading from database
-            this.setState({
-                accesspoint: ['test point']
-            })
-
-            // If we want to try to load from database
-            GetAllPoints()
-        }
-
-        GetAllPoints(e){
-            axios.get('http://localhost:3000/accesspoint/get')
-                .then(response => {
-                    if(response.data.length > 0){
-                        this.setState({
-                            id: response.data.map(accesspoint => accesspoint.id)
-                            longitude: response.data.map(accesspoint => accesspoint.longitude)
-                            latitude: response.data.map(accesspoint => accesspoint.latitude)
-                            time: response.data.map(accesspoint => accesspoint.time)
-                        })
-                    }
-                })
-        }
-
-        HidePointByDate(e){
-            ...
-        }
-
-        ShowPointByDate(e){
-            ...
-        }
-
-        PostPoint(e){
-            axios.post ...
-        }
-    */
-
-
-
-
-
-    render() {
-        return (
-            <html>
-                <header>
-                </header>
-                <body>
-                    <div>Map</div>
-
-                    // <slider
-                    //    className="slider-main-slider"
-                    //    onSlide = {this.ShowPointByDate}
-                    // />
-
-                </body>
-            </html>
-        )
-    }
-}
+export default Map;
